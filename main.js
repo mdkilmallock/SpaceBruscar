40f5a06ce58e4e04ac74186a6e8f772c6afac7e8
Mike Donegan <mikeydonegan@gmail.com>
2021-10-03 13:45:04 +0100

    three.js fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from './node_modules/three/build/three.module.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as satellite from 'satellite.js';
 
